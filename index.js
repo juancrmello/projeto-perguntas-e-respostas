bc56b5156125fe01e41fe4898fd46cf1ec5177fc
Juan Mello <juanribeirofx@gmail.com>
2021-06-26 18:00:38 -0300

    Exibindo variaveis no HTML

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const app = express();
 // Estou dizendo para o Express usar o EJs como View Engine
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/:nome/:lang', (req, res) => {
 
-    res.render('home');
+    var nome = req.params.nome;
+
+    var lang = req.params.lang;
+
+    res.render('index', {
+        nome,
+        lang,
+        empresa: "juancrmello",
+        inscritos: 100000
+    });
 
 });
 
