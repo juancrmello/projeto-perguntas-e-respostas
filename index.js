1b4002e800e848969c2ace221cb0f4587351027b
Juan Mello <juanribeirofx@gmail.com>
2021-06-26 19:09:59 -0300

    Arquivos estaticos

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 // Estou dizendo para o Express usar o EJs como View Engine
 app.set('view engine', 'ejs');
+// Estou dizendo para o Express que permita a utilização de arquivos estáticos, como css, imgs e etc
+app.use(express.static('public'));
 
 app.get('/:nome/:lang', (req, res) => {
 
