6427c47f44c6b5a4f3a525f1f521540a726e58aa
Juan Mello <juanribeirofx@gmail.com>
2021-06-27 18:55:19 -0300

    Criando formulario de perguntas

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-
     res.render('index');
-
 });
 
+app.get('/perguntar', (req, res) => {
+    res.render('perguntar');
+})
 app.listen(3000, () => {console.log("App rodando!")});
