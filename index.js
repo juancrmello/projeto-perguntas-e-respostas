17ddeaaf05d0c139f4ce17f0c371428d1ab37b1a
Juan Mello <juanribeirofx@gmail.com>
2021-06-30 23:51:01 -0300

    Capturando dados de um formulario

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Estou dizendo para o Express usar o EJs como View Engine
 app.set('view engine', 'ejs');
 // Estou dizendo para o Express que permita a utilização de arquivos estáticos, como css, imgs e etc
 app.use(express.static('public'));
+// Comando para conseguir ler os dados enviados para o express
+app.use(bodyParser.urlencoded({extended: false}));
+// Permite trabalhar com json
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -15,8 +20,9 @@ app.get('/perguntar', (req, res) => {
 });
 
 app.post('/salvarpergunta', (req, res) => {
-
-    res.send('Formulário recebido!');
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    res.send(`Formulário recebido! Título: ${titulo} e Descrição: ${descricao}`);
 
 })
 app.listen(3000, () => {console.log("App rodando!")});
