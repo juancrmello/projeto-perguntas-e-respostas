f981718dc688b7220b4e08e232b1c02f136fb941
Juan Mello <juanribeirofx@gmail.com>
2021-07-04 16:44:39 -0300

    Exibição de respostas e finalização

--- index.js
+++ index.js
@@ -50,9 +50,17 @@ app.get("/pergunta/:id", (req, res) => {
         where: { id: id }
     }).then(pergunta => {
         if (pergunta != undefined) { // Pergunta encontrada
-            res.render('pergunta', {
-                pergunta: pergunta
+            
+            Resposta.findAll({
+                where: { perguntaId : pergunta.id },
+                order: [['id', 'DESC']]
+            }).then(respostas => {
+                res.render('pergunta', {
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
             });
+            
         } else { // Pergunta não encontrada
             res.redirect('/');
         }
